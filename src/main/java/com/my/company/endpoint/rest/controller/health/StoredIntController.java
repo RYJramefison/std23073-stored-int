c9acacdc12a16f49b2e5081bed0c12b2f49acead
juninho <hei.juninho.2@gmail.com>
2025-07-07 14:17:58 +0300

    fix: update getStoredInt method to save random number to stored-int.txt if file does not exist in StoredIntController

--- src/main/java/com/my/company/endpoint/rest/controller/health/StoredIntController.java
+++ src/main/java/com/my/company/endpoint/rest/controller/health/StoredIntController.java
@@ -4,11 +4,29 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Random;
+
 @RestController
 @AllArgsConstructor
 public class StoredIntController {
+
+  private static final Path filePath = Path.of("stored-int.txt");
+
   @GetMapping("/stored-int")
-  public int getStoredInt() {
-    return 42;
+  public synchronized int getStoredInt() throws IOException {
+
+    if (Files.exists(filePath)) {
+      String value = Files.readString(filePath).trim();
+      return Integer.parseInt(value);
+    }
+
+    else {
+      int randomNumber = new Random().nextInt(100);
+      Files.writeString(filePath, String.valueOf(randomNumber));
+      return randomNumber;
+    }
   }
 }
