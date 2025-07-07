57caeedd9ded141b2a60482dc01f3de115ae17fd
juninho <hei.juninho.2@gmail.com>
2025-07-07 14:34:08 +0300

    fix: format code using .\format.bat in CI

--- src/main/java/com/my/company/endpoint/rest/controller/health/StoredIntController.java
+++ src/main/java/com/my/company/endpoint/rest/controller/health/StoredIntController.java
@@ -1,13 +1,12 @@
 package com.my.company.endpoint.rest.controller.health;
 
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Random;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
@@ -21,9 +20,7 @@ public class StoredIntController {
     if (Files.exists(filePath)) {
       String value = Files.readString(filePath).trim();
       return Integer.parseInt(value);
-    }
-
-    else {
+    } else {
       int randomNumber = new Random().nextInt(100);
       Files.writeString(filePath, String.valueOf(randomNumber));
       return randomNumber;
