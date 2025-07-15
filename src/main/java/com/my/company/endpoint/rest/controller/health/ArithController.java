ce62fb015c4d1c262e8522b930f2ff5406348350
juninho <hei.juninho.2@gmail.com>
2025-07-15 13:02:07 +0300

    fix: format

--- src/main/java/com/my/company/endpoint/rest/controller/health/ArithController.java
+++ src/main/java/com/my/company/endpoint/rest/controller/health/ArithController.java
@@ -1,4 +1,14 @@
 package com.my.company.endpoint.rest.controller.health;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ArithController {
+
+  @GetMapping("/add")
+  public Long add(@RequestParam Long a, @RequestParam Long b) {
+    return a + b;
+  }
 }
